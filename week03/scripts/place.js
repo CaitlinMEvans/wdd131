057bfe0744bf4a71dd2f267ccb1c032f9c754870
caitmsevans@gmail.com <caitmsevans@gmail.com>
2024-11-15 20:46:26 -0700

    discover ireland project

---
+++ week03/scripts/place.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const images = [
+        {
+            large: "images/landscape1-large.webp",
+            medium: "images/landscape1-medium.webp",
+            small: "images/landscape1-small.webp",
+            alt: "A beautiful landscape in Ireland"
+        },
+        {
+            large: "images/landscape2-large.webp",
+            medium: "images/landscape2-medium.webp",
+            small: "images/landscape2-small.webp",
+            alt: "A stunning view of the Cliffs of Moher in Ireland"
+        },
+        {
+            large: "images/landscape3-large.webp",
+            medium: "images/landscape3-medium.webp",
+            small: "images/landscape3-small.webp",
+            alt: "Ancient castle ruins on a cliff in Ireland"
+        },
+        {
+            large: "images/landscape4-large.webp",
+            medium: "images/landscape4-medium.webp",
+            small: "images/landscape4-small.webp",
+            alt: "The majestic Cliffs of Moher during sunset"
+        }
+    ];
+
+    let currentIndex = 0;
+    const rotatingImage = document.getElementById("rotating-image");
+
+    function rotateImage() {
+        currentIndex = (currentIndex + 1) % images.length;
+        const currentImage = images[currentIndex];
+
+        // Update the img and source attributes
+        rotatingImage.src = currentImage.small;
+        rotatingImage.alt = currentImage.alt;
+        rotatingImage.parentElement.querySelector('source[media="(min-width: 1000px)"]').srcset = currentImage.large;
+        rotatingImage.parentElement.querySelector('source[media="(min-width: 500px)"]').srcset = currentImage.medium;
+    }
+
+    // Set an interval to change the image every 5 seconds
+    setInterval(rotateImage, 5000);
+});
